24c8f159cd05bffcccb6fa2bc57dbe5388cd2152
Jacob <miracle@localhost.net>
2025-09-06 19:06:28 +0700

    nvim theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,17 @@
 return -- lazy
 {
-    "askfiy/visual_studio_code",
-    priority = 100,
-    config = function()
-        vim.cmd([[colorscheme visual_studio_code]])
-    end,
+    {
+        "https://github.com/RRethy/base16-nvim",
+        lazy = false,
+        config = function()
+            require('base16-colorscheme').setup({
+    base00 = '#181818', base01 = '#262626', base02 = '#4c4c4c', base03 = '#aca98a',
+base04 = '#e7e7e7', base05 = '#c0c0c0', base06 = '#f5f5f5', base07 = '#ffffff',
+base08 = '#ac8a8c', base09 = '#c49ea0', base0A = '#c4c19e', base0B = '#8aac8b',
+base0C = '#8aabac', base0D = '#8f8aac', base0E = '#ac8aac', base0F = '#a06949',
+
+})
+        end,
+    },
 }
+
